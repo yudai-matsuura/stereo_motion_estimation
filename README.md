45e36b2fd4545d9296493018090ae78f9a0fd926
Matsuura Yudai <myudai0306@gmail.com>
2026-06-25 19:51:48 +0900

    add yolo inference

---
+++ README.md
@@ -0,0 +1,8 @@
+make virtual environment
+
+python3 -m venv myenv
+source myenv/bin/activate
+
+Install
+pip install opencv-python
+pip install ultralytics
--- src/object_detection.py
+++ src/object_detection.py
@@ -1,8 +1,22 @@
+from ultralytics import YOLO
 import cv2
 
-image_path = "/data/000000_11.png"
+image_path = "data/000000_11.png"
 
 img = cv2.imread(image_path)
 
-print(type(img))
-print(img.shape)
+model = YOLO("yolo26n.pt")
+
+results = model(img)
+result_img = results[0].plot()
+
+cv2.imwrite(
+    "outputs/kitti_detection.png",
+    result_img
+)
+
+print("saved")
+# print(result)
+
+# print(type(img))
+# print(img.shape)
