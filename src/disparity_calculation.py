3d38c30681596da2e0245a3ead996537eb854b7f
Matsuura Yudai <matsuura.yudai.q5@dc.tohoku.ac.jp>
2026-06-27 11:10:31 +0900

    Add pic

---
+++ src/disparity_calculation.py
@@ -0,0 +1,7 @@
+import cv2
+
+image_path_left = "data/left/000000_10.png"
+image_path_right = "data/right/000000_10.png"
+
+left_img = cv2.imread(image_path_left)
+right_img = cv2.imread(image_path_right)
